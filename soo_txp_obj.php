c27c234ac769081e232c63df054469f66fe59b91
Jeff <jeff@ipsedixit.net>
2009-07-05 10:58:59 -0400

    corrected license info

--- soo_txp_obj.php
+++ soo_txp_obj.php
@@ -7,7 +7,7 @@
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
+// the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
 // 
 // This program is distributed in the hope that it will be useful,
@@ -18,7 +18,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-$plugin['version'] = '1.0.a.6';
+$plugin['version'] = '1.0.b.1';
 $plugin['author'] = 'Jeff Soo';
 $plugin['author_uri'] = 'http://ipsedixit.net/';
 $plugin['description'] = 'Object classes for Txp plugins';
